46fada9b19fb036924d97ad347da56c06c9871f1
shalkm123 <km.shal0123@gmail.com>
2026-05-02 18:02:40 +0530

    changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,9 @@
+import { neonConfig } from "@neondatabase/serverless";
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
